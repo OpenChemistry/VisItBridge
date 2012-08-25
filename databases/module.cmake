c01fbf6fb2460ea388f4654f4c112bbd6c736d3f
Utkarsh Ayachit <utkarsh.ayachit@kitware.com>
2012-08-25 15:56:43 -0400

    BUG #13393. Enable GMV reader only when OpenGLU is available.
    Enable GMV reader only when GLU is available. Also the option to optionally turn
    ON GMV reader didn't seem to work. Despite the fact the the GMV reader was
    turned on, it wasn't being built causing dashboard failures. This patch
    overcomes the issue by not making GMV user-optional, instead simply disabling it
    automatically when GLU was not available.

--- databases/module.cmake
+++ databases/module.cmake
@@ -13,6 +13,20 @@ vtk_module(vtkIOVisItBridge
 set_property (GLOBAL PROPERTY
   vtkIOVisItBridge_SERVERMANAGER_XMLS
   ${CMAKE_CURRENT_LIST_DIR}/visit_readers.xml)
+
 set_property (GLOBAL PROPERTY
   vtkIOVisItBridge_PARAVIEW_GUI_XMLS
   ${CMAKE_CURRENT_LIST_DIR}/visit_readers_gui.xml)
+
+#-----------------------------------------------------------------------------
+# GMV Reader needs OpenGLU library. So check if it exists before enabling the
+# GMV reader.
+find_package(OpenGL)
+if (NOT OPENGL_GLU_FOUND)
+  message(STATUS "Disabling VisIt GMV reader since GLU library not found.")
+endif()
+if (OPENGL_GLU_FOUND)
+  set_property (GLOBAL APPEND PROPERTY
+    vtkIOVisItBridge_SERVERMANAGER_XMLS
+    ${CMAKE_CURRENT_LIST_DIR}/visit_readers_gmv.xml)
+endif()
