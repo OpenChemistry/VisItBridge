3495678a93fc245ed03edf9eb4bc7d1f48cf8042
Ben Boeckel <ben.boeckel@kitware.com>
2019-03-07 14:57:51 -0500

    avt: update for mpi linking

--- scripts/update-avt.sh
+++ scripts/update-avt.sh
@@ -8,7 +8,7 @@ readonly name="visit"
 readonly ownership="VisIt Upstream <kwrobot@kitware.com>"
 readonly subtree="Library/VisItLib"
 readonly repo="https://gitlab.kitware.com/third-party/visit.git"
-readonly tag="for/paraview-20190305-trunk-r30784"
+readonly tag="for/paraview-20190307-trunk-r30784"
 
 readonly paths="
 COPYRIGHT
