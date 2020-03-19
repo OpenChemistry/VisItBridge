95a3d70363041b96ac4b9c883102c2ef9c307da1
Yohann Bearzi <yohann.bearzi@kitware.com>
2020-03-19 14:11:17 -0400

    updating avt script

--- scripts/update-avt.sh
+++ scripts/update-avt.sh
@@ -8,7 +8,7 @@ readonly name="visit"
 readonly ownership="VisIt Upstream <kwrobot@kitware.com>"
 readonly subtree="Library/VisItLib"
 readonly repo="https://gitlab.kitware.com/third-party/visit.git"
-readonly tag="for/paraview-20200205-trunk-r34006"
+readonly tag="for/paraview-20200317-trunk-r34006"
 
 readonly paths="
 COPYRIGHT
